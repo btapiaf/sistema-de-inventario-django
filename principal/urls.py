dbbdd04fc14951d1d97290fc6d3d6b6ccd858a87
btapiaf <btapiaf@hotmail.com>
2021-08-26 10:37:56 -0500

    first commit

---
+++ principal/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.contrib.auth import views as auth_view
+from.views import Home
+urlpatterns = [
+    path("",Home.as_view(), name="home"),
+    path('login/',auth_view.LoginView.as_view(template_name='principal/login.html'), name="login"),
+    path('logout/', auth_view.LogoutView.as_view(template_name = "principal/login.html"),name="logout")
+]
---
+++ principal/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+
+class Home(LoginRequiredMixin,generic.TemplateView):
+    template_name = "principal/home.html"
+    login_url = 'principal:login'
